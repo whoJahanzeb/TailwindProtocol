0c383a4b4dc2ddd6f651c24b77eedd83ea33ed69
Muhammad Jahanzeb <i.am.jahanzeb6@gmail.com>
2024-08-08 21:41:28 +0500

    set responsiveness

--- src/Pages/Api.jsx
+++ src/Pages/Api.jsx
@@ -1,51 +1,101 @@
-import React from 'react'
-import {NavLink} from "react-router-dom";
-import {BsArrowRight} from "react-icons/bs";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { BsArrowRight } from "react-icons/bs";
 import GuidesPart from "../components/guidesPart.jsx";
 import ResourcesPart from "../components/resourcesPart.jsx";
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 
 const Api = () => {
-    const {mode} = useSelector(state => state.darkMode)
-    return (
-        <>
-            <main className={mode?"max-w-5xl mx-auto py-20":"max-w-5xl mx-auto py-20"}>
-                <div className="px-10">
-                    <div><h1 className="text-2xl font-semibold">API Documentation</h1></div>
-                    <div className="my-4">
-                        <p>Use the Protocol API to access contacts, conversations, group messages, and more and
-                            seamlessly integrate your product into the workflows of dozens of devoted Protocol
-                            users.</p>
-                    </div>
-                    <div className="space-x-4 my-8 flex">
-                        <NavLink to="/quickstart"
-                                 className={"bg-emerald-300 font-semibold flex items-center text-emerald-800 rounded-full px-2 py-1"}>Quickstart
-                            <BsArrowRight/>
-                        </NavLink>
-                        <NavLink to="/sdks" className={mode?"border rounded-full border-zinc-700 px-2 py-1 text-xs":"text-xs border rounded-full px-2 py-1"}>Explore SDKs</NavLink>
-                    </div>
-                    <div className="my-20">
-                        <div><p className="text-lg font-semibold">Getting started</p></div>
-                        <div>
-                            <p>To get started, create a new application in your <span
-                                className="text-emerald-500 font-semibold">developer settings</span>, then read about
-                                how to make requests for the resources you need to access using our HTTP APIs or
-                                dedicated client SDKs. When your integration is ready to go live, publish it to
-                                our <span className="text-emerald-500 font-semibold">integrations directory</span> to
-                                reach the Protocol community.</p>
-                        </div>
-                        <div>
-                            <NavLink to="sdks"
-                                     className="flex items-center space-x-4 my-4 text-emerald-500 font-semibold">
-                                <div>Get your API key</div>
-                                <BsArrowRight/></NavLink>
-                        </div>
-                    </div>
-                    <GuidesPart/>
-                    <ResourcesPart/>
-                </div>
-            </main>
-        </>
-    )
-}
-export default Api
+  const { mode } = useSelector((state) => state.darkMode);
+  return (
+    <>
+      <main
+        className={mode ? "max-w-5xl mx-auto py-20" : "max-w-5xl mx-auto py-20"}
+      >
+        <div className="px-10">
+          <div>
+            <h1 className="text-2xl font-semibold">API Documentation</h1>
+          </div>
+          <div className="my-4">
+            <p>
+              Use the Protocol API to access contacts, conversations, group
+              messages, and more and seamlessly integrate your product into the
+              workflows of dozens of devoted Protocol users.
+            </p>
+          </div>
+          <div className="space-x-4 my-8 flex">
+            <NavLink
+              to="/quickstart"
+              className={
+                mode
+                  ? "bg-zinc-800 font-semibold flex items-center rounded-full px-2 py-1"
+                  : "bg-zinc-200 font-semibold flex items-center rounded-full px-2 py-1"
+              }
+            >
+              Quickstart
+              <BsArrowRight />
+            </NavLink>
+            <NavLink
+              to="/sdks"
+              className={
+                mode
+                  ? "border rounded-full border-zinc-700 px-2 py-1 text-xs"
+                  : "text-xs border rounded-full px-2 py-1"
+              }
+            >
+              Explore SDKs
+            </NavLink>
+          </div>
+          <div className="my-20">
+            <div>
+              <p className="text-lg font-semibold">Getting started</p>
+            </div>
+            <div>
+              <p>
+                To get started, create a new application in your{" "}
+                <span
+                  className={
+                    mode
+                      ? "text-zinc-200 font-semibold"
+                      : "text-zinc-900 font-semibold"
+                  }
+                >
+                  developer settings
+                </span>
+                , then read about how to make requests for the resources you
+                need to access using our HTTP APIs or dedicated client SDKs.
+                When your integration is ready to go live, publish it to our{" "}
+                <span
+                  className={
+                    mode
+                      ? "text-zinc-200 font-semibold"
+                      : "text-zinc-900 font-semibold"
+                  }
+                >
+                  integrations directory
+                </span>{" "}
+                to reach the Protocol community.
+              </p>
+            </div>
+            <div>
+              <NavLink
+                to="sdks"
+                className={
+                  mode
+                    ? "flex items-center space-x-4 my-4 text-zinc-200 font-semibold"
+                    : "flex items-center space-x-4 my-4 text-zinc-900 font-semibold"
+                }
+              >
+                <div>Get your API key</div>
+                <BsArrowRight />
+              </NavLink>
+            </div>
+          </div>
+          <GuidesPart />
+          <ResourcesPart />
+        </div>
+      </main>
+    </>
+  );
+};
+export default Api;
--- src/components/guidesPart.jsx
+++ src/components/guidesPart.jsx
@@ -1,74 +1,106 @@
-import React from 'react'
-import {NavLink} from "react-router-dom";
-import {BsArrowRight} from "react-icons/bs";
-import {useSelector} from "react-redux";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { BsArrowRight } from "react-icons/bs";
+import { useSelector } from "react-redux";
 
 const GuidesPart = () => {
-    const {mode} = useSelector(state => state.darkMode)
-    return (
-        <>
-            <div className={mode ? "border-b my-20 border-zinc-700" : "border-b my-20"}>
-                <p className="my-4 font-semibold text-lg">Guides</p>
-            </div>
-            <div className="md:flex gap-10 my-4">
-                <div>
-                    <div>
-                        <h2 className="font-semibold text-sm">Authentication</h2>
-                    </div>
-                    <div>
-                        <p>Learn how to authenticate your API requests.</p>
-                    </div>
-                    <div>
-                        <NavLink to="authentication" className="flex items-center text-emerald-500 font-semibold">Read
-                            more
-                            <BsArrowRight className="text-sm"/>
-                        </NavLink>
-                    </div>
-                </div>
-                <div>
-                    <div>
-                        <h2 className="font-semibold text-sm">Pagination</h2>
-                    </div>
-                    <div>
-                        <p>Understand how to work with paginated responses.
-
-                        </p>
-                    </div>
-                    <div>
-                        <NavLink to="pagination" className="flex items-center text-emerald-500 font-semibold">Read more
-                            <BsArrowRight className="text-sm"/>
-                        </NavLink>
-                    </div>
-                </div>
-                <div>
-                    <div>
-                        <h2 className="font-semibold text-sm">Errors</h2>
-                    </div>
-                    <div>
-                        <p>Read about the different types of errors returned by the API.</p>
-                    </div>
-                    <div>
-                        <NavLink to="errors" className="flex items-center text-emerald-500 font-semibold">Read more
-                            <BsArrowRight className="text-sm"/>
-                        </NavLink>
-                    </div>
-                </div>
-                <div>
-                    <div>
-                        <h2 className="font-semibold text-sm">Webhooks</h2>
-                    </div>
-                    <div>
-                        <p>Learn how to programmatically configure webhooks for your app.</p>
-                    </div>
-                    <div>
-                        <NavLink to="webhooks"
-                                 className="flex items-center content-center text-emerald-500 font-semibold">Read more
-                            <BsArrowRight className="text-sm"/>
-                        </NavLink>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
-export default GuidesPart
+  const { mode } = useSelector((state) => state.darkMode);
+  return (
+    <>
+      <div
+        className={mode ? "border-b my-20 border-zinc-700" : "border-b my-20"}
+      >
+        <p className="my-4 font-semibold text-lg">Guides</p>
+      </div>
+      <div className="md:flex gap-10 my-4">
+        <div>
+          <div>
+            <h2 className="font-semibold text-sm">Authentication</h2>
+          </div>
+          <div>
+            <p>Learn how to authenticate your API requests.</p>
+          </div>
+          <div>
+            <NavLink
+              to="authentication"
+              className={
+                mode
+                  ? "flex items-center text-zinc-200 font-semibold"
+                  : "flex items-center text-zinc-900 font-semibold"
+              }
+            >
+              Read more
+              <BsArrowRight className="text-sm" />
+            </NavLink>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h2 className="font-semibold text-sm">Pagination</h2>
+          </div>
+          <div>
+            <p>Understand how to work with paginated responses.</p>
+          </div>
+          <div>
+            <NavLink
+              to="pagination"
+              className={
+                mode
+                  ? "flex items-center text-zinc-200 font-semibold"
+                  : "flex items-center text-zinc-900 font-semibold"
+              }
+            >
+              Read more
+              <BsArrowRight className="text-sm" />
+            </NavLink>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h2 className="font-semibold text-sm">Errors</h2>
+          </div>
+          <div>
+            <p>Read about the different types of errors returned by the API.</p>
+          </div>
+          <div>
+            <NavLink
+              to="errors"
+              className={
+                mode
+                  ? "flex items-center text-zinc-200 font-semibold"
+                  : "flex items-center text-zinc-900 font-semibold"
+              }
+            >
+              Read more
+              <BsArrowRight className="text-sm" />
+            </NavLink>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h2 className="font-semibold text-sm">Webhooks</h2>
+          </div>
+          <div>
+            <p>
+              Learn how to programmatically configure webhooks for your app.
+            </p>
+          </div>
+          <div>
+            <NavLink
+              to="webhooks"
+              className={
+                mode
+                  ? "flex items-center text-zinc-200 font-semibold"
+                  : "flex items-center text-zinc-900 font-semibold"
+              }
+            >
+              Read more
+              <BsArrowRight className="text-sm" />
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default GuidesPart;
